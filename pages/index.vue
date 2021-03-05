cedb68eaef9512dbd10897caa6c0456c8f11b6b0
nim173 <nchathuranga173@gmail.com>
2021-03-05 12:26:27 +0530

    initial

---
+++ pages/index.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <h2>Welcome to DadJokes</h2>
+  </div>
+</template>
+
+<script>
+export default {
+  head() {
+    return {
+        title: 'Welcome',
+        meta: [
+            {
+                hid: 'description',
+                name: 'description',
+                content: 'Best place for corny dad jokes'
+            }
+        ]
+    }
+  }
+}
+</script>
